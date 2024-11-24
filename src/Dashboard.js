816c3955d45edef8bb0469931aa6bbff532504bd
Amaan <amaanshaikh.gg@gmail.com>
2024-11-24 23:37:53 +0530

    initial commit

---
+++ src/Dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div className="p-5">
+      <h1>Welcome to the RBAC System</h1>
+      <p>Use the navigation menu to manage users, roles, and permissions.</p>
+    </div>
+  );
+};
+
+export default Dashboard;
